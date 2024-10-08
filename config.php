27781a489843d76f7784dd5d308dc3b7e0262ad0
mahaardhiika <46954102+mahaardhiika@users.noreply.github.com>
2024-10-08 22:12:58 +0700

    Create config.php

---
+++ config.php
@@ -0,0 +1,29 @@
+<?php
+// config.php
+
+// List of subdomains
+$subdomains = [];
+for ($i = 1; $i <= 99; $i++) {
+    $subdomains[] = "dev-$i.bersiber.xyz"; //dev-11.bersiber.xyz ganti ke dev- lain
+}
+
+// Dummy email and password
+$credentials = [
+    "email" => "ricko-ricko@mail.ru",
+    "password" => "-"
+];
+
+// Database simulation (array)
+$users = [
+    [
+        'email' => 'ricko-ricko@mail.ru',
+        'password' => '',
+        'name' => 'Ricko'
+    ],
+    [
+        'email' => 'jane-doe@mail.ru',
+        'password' => '',
+        'name' => 'Jane Doe'
+    ]
+];
+?>
